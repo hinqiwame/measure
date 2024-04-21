67fdf6842347f53a1fc1a0432c63e8852fa5c46f
Mathias Lindström <Q51RpjCmv5Lz@proton.me>
2024-04-21 21:40:39 +0300

    Create install-measure.sh

---
+++ installer/install-measure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Measure binary here
+# Might be something like https://github.com/hinqiwame/measure/releases/latest/measure ?
+curl -LJO https://github.com/ 
+
+if [ $? -eq 0 ]; then
+    sudo mv measure /usr/bin/
+    echo "Measure installed."
+else
+    echo "Failed to install measure."
+fi
